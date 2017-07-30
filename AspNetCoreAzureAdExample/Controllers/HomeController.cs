091bdcb4638df37443f6fa1b419e08bfb623c92e
Christian Sparre <christian@sparre.it>
2017-07-30 12:58:36 +0200

    Add simple Azure AD example

---
+++ AspNetCoreAzureAdExample/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreAzureAdExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        [HttpGet("secret-place")]
+        public IActionResult VerySecretPlace()
+        {
+            return Content($"Welcome {User.Identity.Name}");
+        }
+    }
+}
---
+++ AspNetCoreAzureAdExample/Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreAzureAdExample
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(o =>
+            {
+                o.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                o.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                o.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            });
+
+            services.AddOpenIdConnectAuthentication(o =>
+            {
+                var config = _configuration.GetSection("AzureAdAuthentication");
+
+                o.ClientId = config["ClientId"];
+                o.Authority = $"{config["Instance"]}{config["TenantId"]}";
+                o.UseTokenLifetime = true;
+                o.CallbackPath = config["CallbackPath"];
+            });
+
+            services.AddCookieAuthentication();
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
